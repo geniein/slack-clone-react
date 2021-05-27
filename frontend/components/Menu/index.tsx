8877ca4207c3353536abed19771fb9570d50e8ad
geniein <cubya0104@gmail.com>
2021-05-27 23:27:03 +0900

    Menu

--- frontend/components/Menu/index.tsx
+++ frontend/components/Menu/index.tsx
@@ -1,19 +1,27 @@
-import React, { CSSProperties, FC } from 'react';
+import React, { CSSProperties, FC, useCallback } from 'react';
 import {CloseModalButton, CreateMenu} from './styles';
 interface Props {
     show: boolean;
-    onCloseModal: () => void;
+    onCloseModal: (e: any) => void;
     style:CSSProperties;
     closeButton?: boolean;
 }
 const Menu: FC<Props> =({children, style, show, onCloseModal, closeButton}) =>{
+    const stopPropagation = useCallback(
+        (e) => {
+            e.stopPropagation();
+        },
+        [],
+    )
+    if(!show){
+        return null;
+    }
     return(
         <CreateMenu onClick={onCloseModal}>
-            <div style={style}>
+            <div style={style} onClick={stopPropagation}>
                 {closeButton &&<CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
                 {children}                
-            </div>  
-            {children}    
+            </div>              
         </CreateMenu>
     );
 };
